d0609d02799b98eb45312dee0b8c95ed38de8d60
8mmfstop2-ops <8mmfstop2@gmail.com>
2025-12-29 18:35:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1051,7 +1051,7 @@ app.post("/api/player/join", async (req, res) => {
 
     // Redirect to game table
     return res.json({
-      redirect: `/table/${code}?playerId=${playerId}`
+      redirect: `/room/${code}?playerId=${playerId}`
     });
 
   } catch (err) {
@@ -1195,5 +1195,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => console.log("Server running on port", PORT));
+
 
 
